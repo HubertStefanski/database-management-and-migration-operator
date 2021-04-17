aa6a905f3110f47bb27a2e7abc360525ca8caa0f
Hubert Stefanski <hstefans@redhat.com>
2021-04-17 19:40:24 +0100

    small cleanup

--- controllers/mysql/dbmmomysql_controller.go
+++ controllers/mysql/dbmmomysql_controller.go
@@ -42,15 +42,6 @@ type DBMMOMySQLReconciler struct {
 // +kubebuilder:rbac:groups=cache.my.domain,resources=dbmmomysqls/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=cache.my.domain,resources=dbmmomysqls/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DBMMOMySQL object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.0/pkg/reconcile
 func (r *DBMMOMySQLReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	ctx = context.Background()
 	log := r.Log.WithValues(constants.MysqlControllerName, req.NamespacedName)
